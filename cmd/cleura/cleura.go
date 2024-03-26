7743db70f97d89804b2f74801fc5cb4e84660b6b
Leonid Zaikin <lza@aztek.no>
2024-03-26 10:53:10 +0000

    chore: add build and release workflow

--- cmd/cleura/cleura.go
+++ cmd/cleura/cleura.go
@@ -14,6 +14,7 @@ import (
 
 var (
 	version = "dev"
+	commit  = "uncommitted"
 )
 
 func commands() []*cli.Command {
@@ -31,7 +32,7 @@ func main() {
 	app := cli.NewApp()
 	app.Usage = "A Cleura API CLI"
 	app.Name = "cleura"
-	app.Version = version
+	app.Version = version + "-" + commit
 	app.Commands = commands()
 	err := app.Run(os.Args)
 	if err != nil {
